51ce8e2f3373cf77466e93ac6d3e27f6576d6965
abas <abdelrahmanali2310@gmail.com>
2024-11-11 11:52:28 +0200

    end of home screen and add font style and mange screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce/screens/home.dart';
+import 'package:e_commerce/screens/splash_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: SplashView(),
     );
   }
 }
